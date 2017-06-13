57fd794697e78e94a8855e379cf51f31a5283f0a
hshare <1015918929@qq.com>
2017-06-13 17:38:35 +0800

    修改DialogPopup

--- library/src/main/java/razerdp/demo/popup/DialogPopup.java
+++ library/src/main/java/razerdp/demo/popup/DialogPopup.java
@@ -17,24 +17,68 @@ import razerdp.basepopup.BasePopupWindow;
  * dialogpopup :)
  * 客串一下dialog
  */
-public class DialogPopup extends BasePopupWindow implements View.OnClickListener{
+public class DialogPopup extends BasePopupWindow implements View.OnClickListener {
 
     private TextView ok;
     private TextView cancel;
+    private TextView title;
+    private TextView content;
+
+    private OnDialogPopupItemClickListener onDialogPopupItemClickListener;
 
     public DialogPopup(Activity context) {
         super(context);
 
-        ok= (TextView) findViewById(R.id.ok);
-        cancel= (TextView) findViewById(R.id.cancel);
+        ok = (TextView) findViewById(R.id.ok);
+        cancel = (TextView) findViewById(R.id.cancel);
 
-        setViewClickListener(this,ok,cancel);
+        setViewClickListener(this, ok, cancel);
+    }
+
+    public DialogPopup setTitleText(String s) {
+        title.setText("" + s);
+        return this;
+    }
+
+    public DialogPopup setContentText(String s) {
+        content.setText("" + s);
+        return this;
+    }
+
+    public DialogPopup setLeftText(String s) {
+        ok.setText("" + s);
+        return this;
+    }
+
+    public DialogPopup setRightText(String s) {
+        cancel.setText("" + s);
+        return this;
+    }
+
+    public DialogPopup setTitleColor(int s) {
+        title.setTextColor(s);
+        return this;
+    }
+
+    public DialogPopup setContentColor(int s) {
+        content.setTextColor(s);
+        return this;
+    }
+
+    public DialogPopup setLeftColor(int s) {
+        ok.setTextColor(s);
+        return this;
+    }
+
+    public DialogPopup setRightColor(int s) {
+        cancel.setTextColor(s);
+        return this;
     }
 
     @Override
     protected Animation initShowAnimation() {
-        AnimationSet set=new AnimationSet(false);
-        Animation shakeAnima=new RotateAnimation(0,15,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        AnimationSet set = new AnimationSet(false);
+        Animation shakeAnima = new RotateAnimation(0, 15, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         shakeAnima.setInterpolator(new CycleInterpolator(5));
         shakeAnima.setDuration(400);
         set.addAnimation(getDefaultAlphaAnimation());
@@ -60,14 +104,23 @@ public class DialogPopup extends BasePopupWindow implements View.OnClickListener
     @Override
     public void onClick(View v) {
         int i = v.getId();
-        if (i == R.id.ok) {
-            Toast.makeText(getContext(), "click the ok button", Toast.LENGTH_SHORT).show();
-
-        } else if (i == R.id.cancel) {
-            Toast.makeText(getContext(), "click the cancel button", Toast.LENGTH_SHORT).show();
-
-        } else {
+        if (onDialogPopupItemClickListener != null){
+            if (i == R.id.ok) {
+                onDialogPopupItemClickListener.onLeftClick();
+            } else if (i == R.id.cancel) {
+                onDialogPopupItemClickListener.onRightClick();
+            } else {
+            }
         }
+    }
 
+    public void setOnDialogPopupItemClickListener(OnDialogPopupItemClickListener onDialogPopupItemClickListener) {
+        this.onDialogPopupItemClickListener = onDialogPopupItemClickListener;
+    }
+
+    public interface OnDialogPopupItemClickListener {
+        void onLeftClick();
+
+        void onRightClick();
     }
 }
